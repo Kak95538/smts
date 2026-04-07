3acf47b36dbf520e0fdfb10af5e13e53beb8402e
Anil Kumar <anilkumarkadiyam4@gmail.com>
2026-04-07 14:09:14 +0530

    Initial commit: auth-service + task-service

--- task-service/src/main/java/com/example/stms/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.stms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.stms.entity.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
